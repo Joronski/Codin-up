26e03b3258b346153c0554ff7414d8af1f46672d
Joronski <91183608+Joronski@users.noreply.github.com>
2022-11-06 22:34:26 +0800

    Delete JS directory

--- JS/List of Course/HTML Course.js
+++
@@ -1,9 +0,0 @@
-// Not bringing sidebar when active
-const menu_toggle = document.querySelector('.menu-toggle');
-const sidebar = document.querySelector('.sidebar');
-
-// When the button is clicked the sidebar will be active in the following code
-menu_toggle.addEventListener('click', () => {
-  menu_toggle.classList.toggle('is-active');
-  sidebar.classList.toggle('is-active');
-})
